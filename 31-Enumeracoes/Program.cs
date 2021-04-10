3520f9dc7927b874583751b97762c5c4e7004272
juliananovais08 <juliana_novais08@hotmail.com>
2021-04-10 20:45:17 -0300

    aula 31

---
+++ 31-Enumeracoes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _31_Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); NÃO FAZER ASSIM
+
+            //LEMBRETE:Fazer conversão de string para enumeração assim:
+
+            //OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered")
+
+            //ou assim:
+
+            //OrderStatus os;
+            //Enum.TryParse("Delivered", out os);
+        }
+    }
+}
