c349f4b5fcc1fcf1da10cf8c8b9a3c75a4e0cdef
juliananovais08 <juliana_novais08@hotmail.com>
2021-04-10 20:16:03 -0300

    gitignore fix

--- 26-DateTime/Program.cs
+++ 26-DateTime/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _26_DateTime
 {
@@ -6,7 +7,42 @@ namespace _26_DateTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime d0 = DateTime.Now;
+            Console.WriteLine(d0);
+            Console.WriteLine(d0.Ticks);
+
+            DateTime d1 = new DateTime(2000, 8, 15);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58, 275);
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.UtcNow;
+            DateTime d6 = DateTime.Today;
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+
+
+            DateTime d7 = DateTime.Parse("2000-08-15");
+            DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d9 = DateTime.Parse("15/08/2000");
+            DateTime d10 = DateTime.Parse("15/08/2000 13:05:58");
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+
+            DateTime d11 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd",
+            CultureInfo.InvariantCulture);
+
+            DateTime d12 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss",
+            CultureInfo.InvariantCulture);
+
+            Console.WriteLine(d11);
+            Console.WriteLine(d12);
         }
     }
 }
---
+++ 277-TimeSpan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _277_TimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
